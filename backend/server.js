88bc64c957ba11a976e49d65a727a12d84307fd7
riya1689 <riyaratri24@gmail.com>
2025-07-24 15:44:37 +0600

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors"); // <--- 1. ADD THIS LINE
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// --- 2. ADD THESE TWO LINES ---
+app.use(cors()); // This allows my Vercel frontend to make requests
 app.use(express.json());
+// ------------------------------
 
 // Connect to MongoDB
 connectDB();
